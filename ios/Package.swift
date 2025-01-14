fb3f3ee64d235dc751e71eb4d208e8a2cfcfb1cc
Harry <hari2013gupta@users.noreply.github.com>
2025-01-14 12:51:47 +0530

    Create Package.swift
    Support SPM - Swift Package Manager

---
+++ ios/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "jio_inet",
+    platforms: [.iOS(.v10)],
+    products: [
+        .library(
+            name: "jio_inet",
+            targets: ["jio_inet"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "jio_inet",
+            dependencies: [],
+            resources: [
+                .process("PrivacyInfo.xcprivacy"),
+            ],
+            path: "path/to/source")
+    ]
+)
